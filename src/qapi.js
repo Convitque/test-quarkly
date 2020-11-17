8e20a110922f009707e47aaa914fb3444fdf3a92
Convitque <47343818+Convitque@users.noreply.github.com>
2020-11-17 09:03:56 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb32ea0b1c8af001eb54132",
+				"5fb32ea0b1c8af001eb54134"
+			]
+		},
+		"5fb32ea0b1c8af001eb54132": {
+			"id": "5fb32ea0b1c8af001eb54132",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb32ea0b1c8af001eb54134": {
+			"id": "5fb32ea0b1c8af001eb54134",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
